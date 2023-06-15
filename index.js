273b0779b21cb7f5ab047b8381c887de91e67157
TARUN <87805937+TARUNTINKU363@users.noreply.github.com>
2023-06-15 10:03:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+var Name = prompt("Hello welcome 🙂 ! Name Please ? ");
+
+alert("Okay "+ Name + ", Let's play Dicee game..! , I will be Player-2 🦊");
+
+
+var randomNumber1 =(Math.floor(Math.random() * 6)) + 1
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSource = randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = (Math.floor(Math.random() * 6)) + 1 ;
+
+var randomDiceSource2 = "dice" + randomNumber2 + ".png";
+
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomDiceSource2);
+
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
+
+
+
+
